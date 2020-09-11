99303965f3e33ce8084b3ede10055dbd39949216
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-09-10 19:35:56 -0600

    created a function to get the game data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,61 @@
-var API_KEY = config.API_KEY;
-console.log(API_KEY);
+var topTenBoxEl = document.querySelector("#top-ten");
 
-var displayTopFive = function() {
-    fetch(`https://api-v3.igdb.com/age_ratings`)
-    .then(response => {
-        console.log(response.data);
+var getTopTen = function () {
+    fetch("https://rawg-video-games-database.p.rapidapi.com/games", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
+            "x-rapidapi-key": "b26c93ba6cmshec93fde4486eaf6p1c7506jsn8451a97d31ae"
+        }
     })
-    .catch(err => {
-        console.error(err);
-    });
-
+        .then(response => {
+            response.json().then(data => {
+                displayTopTen(data.results);
+            })
+        })
+        .catch(err => {
+            console.log(err);
+        });
 }
 
-displayTopFive();
+var displayTopTen = function (gameDataArr) {
+    for (var i = 0; i < 1 && i < gameDataArr.length; i++) {
+        // console.log(gameDataArr[i]);
+
+        var gameBoxEl = document.createElement("div");
+        gameBoxEl.setAttribute("id", gameDataArr[i].id);
+
+        var gameTitleEl = document.createElement("h2");
+        gameTitleEl.textContent = gameDataArr[i].name;
+        gameBoxEl.appendChild(gameTitleEl);
+
+        var gameScoreEl = document.createElement("p");
+        gameScoreEl.textContent = gameDataArr[i].metacritic;
+        gameBoxEl.appendChild(gameScoreEl);
+
+        var gameDetails = getGameDetails(gameDataArr[i]);
+        console.log(gameDetails);
+
+        topTenBoxEl.appendChild(gameBoxEl);
+    }
+}
+
+var getGameDetails = async function (gameData) {
+    fetch(`https://rawg-video-games-database.p.rapidapi.com/games/${gameData.slug}`, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
+            "x-rapidapi-key": "b26c93ba6cmshec93fde4486eaf6p1c7506jsn8451a97d31ae"
+        }
+    })
+        .then(response => {
+            response.json().then(data => {
+                return data;
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
+
+getTopTen();
